02567411a9a9dcadd61551bd2f2d2e212444f3c1
The New Mr <the_new_mr@yahoo.com>
2019-04-27 00:25:27 +0100

    Use Android KTX doOnLayout instead of GlobalLayoutListener

--- app/src/main/java/com/example/barproblem/MainActivity.kt
+++ app/src/main/java/com/example/barproblem/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.barproblem
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.doOnLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,30 +16,17 @@ class MainActivity : AppCompatActivity() {
         text.text = "Text"
         value.text = "Value"
 
-        val globalLayoutListener = MainActivityGlobalListener(item_view, text, value, percentage_bar)
+        item_view.doOnLayout {
+            Log.i("yoyo", "in onGlobalLayout")
 
-        item_view.viewTreeObserver.addOnGlobalLayoutListener(globalLayoutListener)
-    }
-}
+            val width = value.x - (text.x)
+            Log.i("yoyo", "Width: $width")
 
-class MainActivityGlobalListener(private val itemView: View,
-                                 private val text: View,
-                                 private val value: View,
-                                 private val bar: View) : ViewTreeObserver.OnGlobalLayoutListener {
+            val params: ConstraintLayout.LayoutParams = percentage_bar.layoutParams as ConstraintLayout.LayoutParams
 
-    override fun onGlobalLayout() {
-        Log.i("yoyo", "in onGlobalLayout")
-
-        val width = value.x - (text.x)
-        Log.i("yoyo", "Width: $width")
-
-        val params: ConstraintLayout.LayoutParams = bar.layoutParams as ConstraintLayout.LayoutParams
-
-        params.matchConstraintMaxWidth = (1.0 * width).toInt()  // using 0.1 here - this value is dynamic in real life scenario
-        bar.layoutParams = params
-
-        // required to prevent infinite loop
-        itemView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            params.matchConstraintMaxWidth = (1.0 * width).toInt()  // using 0.1 here - this value is dynamic in real life scenario
+            percentage_bar.layoutParams = params
+        }
     }
 }
 
